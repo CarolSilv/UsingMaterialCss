fc962fc55ef52b13fb88c4e93cccbd78920f7b0d
Carol <csantsilv@gmail.com>
2023-06-30 15:48:49 -0300

    ok

---
+++ views/Home/Home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>AfGeekro!</title>
+    <link rel="stylesheet" href="../../scripts/MaterializeCSS/fonts.googleapis.com_icon_family=Material+Icons.css">
+    <link rel="stylesheet" href="../../scripts/MaterializeCSS/cdnjs.cloudflare.com_ajax_libs_materialize_1.0.0_css_materialize.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./Home.css">
+</head>
+
+<body>
+    <div class="content">
+        <div class="navbar">
+            <?php require "../components/Navbar.php"; ?>
+        </div>
+        <div class="parallax-container">
+            <div class="parallax">
+                <img src="https://wallpapers.com/images/hd/akame-ga-kill-akame-red-aesthetic-xjtmpde2mcnkzn9f.jpg">
+            </div>
+        </div>
+        <div class="section white">
+            <div class="row container">
+                <h2 class="header">AfGeekro</h2>
+                <p class="grey-text text-darken-3 lighten-3">Projetinho para usar o MaterializeCss.</p>
+                <p class="grey-text text-darken-3 lighten-3">E aproveitar para explorar uma das coisas que gosto muito(animes).</p>
+                <p class="grey-text text-darken-3 lighten-3">Com intenção de aprender mais, criei o mundo anime para ser um hobby, algo de interesse meu mesmo mas que também me da a possibilidade de aprender um dos vários frameworks do CSS que é o MaterializeCss.</p>
+            </div>
+        </div>
+        <div class="parallax-container">
+            <div class="parallax">
+                <img src="https://wallpapers.com/images/high/dope-anime-demon-slayer-x464mgjyitdhl1e4.webp">
+            </div>
+        </div>
+    </div>
+
+    <script src="../../scripts/MaterializeCSS/code.jquery.com_jquery-3.2.1.min.js"></script>
+    <script src="../../scripts/MaterializeCSS/cdnjs.cloudflare.com_ajax_libs_materialize_1.0.0_js_materialize.min.js"></script>
+    <script src="./Home.js"></script>
+
+</body>
+
+</html>
---
+++ views/components/Navbar.php
@@ -0,0 +1,12 @@
+<div>
+    <nav>
+        <div class="nav-wrapper blue-grey darken-4">
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Sass</a></li>
+                <li><a href="#">Components</a></li>
+                <li><a href="#">JavaScript</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
